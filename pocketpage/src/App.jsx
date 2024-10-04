07c5c50c73f86a7036f83c2ae6e138f2b80c3019
sonminuk <131163417+sonminuk@users.noreply.github.com>
2024-10-04 21:34:21 +0900

    re

--- pocketpage/src/App.jsx
+++ pocketpage/src/App.jsx
@@ -10,6 +10,10 @@ import NoticeMain from "./components/notice_board/NoticeMain";
 import ToolsList from './components/ToolsList';
 import Home from './Home';
 import PokemonTypeCalculator from './PokemonTypeCalculator';
+import PostForm from "./components/notice_board/PostForm";// 게시판 작성 컴포넌트
+import PostDetail from "./components/notice_board/PostDetail";// 게시판 조회 컴포넌트
+import PostEdit from './components/notice_board/PostEdit'; // 게시판 수정 컴포넌트
+import User from './components/user/userMain'; // 유저 컴포넌트
 import "./App.css";
 
 function App() {
@@ -50,6 +54,7 @@ function App() {
               <li><Link to="/biomes">바이옴</Link></li>
               <li><Link to="/tools">도구</Link></li>
               <li><Link to="/notice">게시판</Link></li>
+               <li><Link to="/user">유저</Link></li> {/* 유저 링크 추가 */}
               <li><Link to="/pokemon-type-calculator">포켓몬 타입 계산기</Link></li>
             </ul>
           </nav>
@@ -65,6 +70,14 @@ function App() {
           <Route path="/tools" element={<ToolsList />} />
           <Route path="/notice" element={<NoticeMain />} />
           <Route path="/pokemon-type-calculator" element={<PokemonTypeCalculator />} />
+           {/* 게시판 관련 라우트 추가 */}
+          <Route path="/notice" element={<NoticeMain />} />
+          <Route path="/postform" element={<PostForm />} />
+          <Route path="/" element={<NoticeMain />} />
+          <Route path="/post/:postId" element={<PostDetail />} /> 
+          <Route path="/postedit/:postId" element={<PostEdit />} /> 
+          {/* 유저 페이지 라우트 추가 */}
+          <Route path="/user" element={<User />} />
         </Routes>
       </div>
     </Router>
